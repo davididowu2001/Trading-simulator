ec97ff67a7fd7bd4b7e0aaba2c33ef6038f73bee
davididowu2001 <idowu.subomi9@gmail.com>
2026-06-20 17:04:01 +0100

    backend mvp done

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/david/trading-simulator/internal/auth"
 	"github.com/david/trading-simulator/internal/database"
+	"github.com/david/trading-simulator/internal/portfolio"
 	"github.com/david/trading-simulator/internal/trade"
 	"github.com/joho/godotenv"
 )
@@ -22,6 +23,10 @@ func main() {
     http.HandleFunc("/auth/login", auth.Login)
 
     http.HandleFunc("/trade/buy", auth.MiddlewareJWT(trade.BuyStock))
+    http.HandleFunc("/trade/sell", auth.MiddlewareJWT(trade.SellStock))
+    http.HandleFunc("/portfolio", auth.MiddlewareJWT(portfolio.Getporfolio))
+
+    http.HandleFunc("/stocks/search", auth.MiddlewareJWT(trade.SearchStock))
     fmt.Println("Server running on port 8080")
     http.ListenAndServe(":8080", nil)
 }
